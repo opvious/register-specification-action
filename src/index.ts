b5e28ec6380f93597544f7bc3fcee71476ec83e5
Matthieu Monsch <matthieu.monsch@gmail.com>
2022-11-03 18:49:05 -0700

    Add dry-run option

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import * as core from '@actions/core';
+import * as glob from '@actions/glob';
 import * as github from '@actions/github';
 import {readFile} from 'fs/promises';
+import path from 'path';
 import {OpviousClient} from 'opvious';
 
 async function main(): Promise<void> {
@@ -9,22 +11,30 @@ async function main(): Promise<void> {
     apiEndpoint: core.getInput('api-endpoint') || undefined,
     hubEndpoint: core.getInput('hub-endpoint') || undefined,
   });
+  const dryRun = core.getBooleanInput('dry-run');
 
-  const formulation = core.getInput(
-    'formulation-name',
-    {required: true, trimWhitespace: true}
-  );
+  const srcGlob = core.getInput('sources', {required: true});
+  const globber = await glob.create(srcGlob);
 
-  const srcPath = core.getInput('source', {required: true});
-  const src = await readFile(srcPath, 'utf8');
-  const defs = await client.extractDefinitions(src);
-  const spec = await client.registerSpecification({
-    formulationName: formulation,
-    definitions: defs,
-    description: src,
-  });
-  core.setOutput('revno', spec.revno);
-  core.setOutput('url', client.specificationUrl(formulation, spec.revno));
+  for await (const srcPath of globber.globGenerator()) {
+    console.log(`Processing ${srcPath}...`);
+    const src = await readFile(srcPath, 'utf8');
+    const {name} = path.parse(srcPath);
+    const defs = await client.extractDefinitions(src);
+    if (dryRun) {
+      await client.validateDefinitions(defs);
+      console.log(`Validated ${srcPath}.`);
+    } else {
+      const spec = await client.registerSpecification({
+        formulationName: name,
+        definitions: defs,
+        description: src,
+      });
+      console.log(`Registered ${srcPath}. [revno=${spec.revno}]`);
+      core.setOutput(name + '-revno', spec.revno);
+      core.setOutput(name + '-url', client.specificationUrl(name, spec.revno));
+    }
+  }
 }
 
 if (module == require.main) {
